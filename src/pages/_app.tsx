ecfcbaa97545548ab0fdcf68cc08dce0e67ebd7a
Omar Ramirez <onmigregor@gmail.com>
2026-06-22 21:58:14 -0400

    feat: initialize master clients list module with types, configuration, and layout boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -121,12 +121,12 @@ const App = (props: ExtendedAppProps) => {
     <Provider store={store}>
       <CacheProvider value={emotionCache}>
         <Head>
-          <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+          <title>{themeConfig.templateName}</title>
           <meta
             name='description'
-            content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+            content={`${themeConfig.templateName} - Admin Dashboard`}
           />
-          <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+          <meta name='keywords' content='Metrics, Smart Polar, Admin, Dashboard' />
           <meta name='viewport' content='initial-scale=1, width=device-width' />
         </Head>
 
